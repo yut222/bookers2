3df8aca890ba6dbc17185fd0cbb3d14dba67050c
EC2 Default User <ec2-user@ip-172-31-6-238.ap-northeast-1.compute.internal>
2023-01-19 15:11:45 +0000

    [Add] 課題3：アプリケーションを作成してみよう：応用編 [レイアウト]★

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,14 @@
 class Book < ApplicationRecord
   has_one_attached :image  
   belongs_to :user
+  
+  
+  
+  has_many :favorite_users, through: :favorites, source: :user
+  has_many :book_comments, dependent: :destroy
+
+  validates :title, presence: true
+  validates :body, presence: true, length: { maximum: 200 }
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
 
 
   resources :books, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :index, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
