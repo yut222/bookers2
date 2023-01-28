ec45247ca3630b0ea3a8f6ddd8ce09dc10a87d6f
EC2 Default User <ec2-user@ip-172-31-6-238.ap-northeast-1.compute.internal>
2023-01-28 07:04:30 +0000

    [Add] 課題3：アプリケーションを作成してみよう：応用編 [RSpecテスト]

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
   end
 
   # バリデーション
-  validates :name, length: { minimum: 2,maximum: 20 }
+  validates :name, uniqueness: true, length: { minimum: 2,maximum: 20 }
   validates :introduction, length: { maximum: 50 }
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  root to: "homes#top"
+  root to: "home#top"
   devise_for :users
-  
-  get 'homes/about' => 'homes#about'
+
+  get 'home/about' => 'home#about'
 
   resources :books, only: [:new, :create, :index, :edit, :show, :update, :destroy]
   resources :users, only: [:show, :index, :edit, :update]
